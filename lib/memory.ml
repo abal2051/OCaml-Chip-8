181ad14a2292fecf37b13580166bdd2d936b751d
Abdullatif <abal2051@colorado.edu>
2020-01-20 17:05:03 -0700

    improve formatting

--- lib/memory.ml
+++ lib/memory.ml
@@ -6,6 +6,6 @@ type long_register = int ref
 let reg_index reg = reg 
 let reg byte = byte
 let long_reg = ref 0 
-(* let registers = Array.create ~len:16 0 *)
+let registers = Array.create ~len:16 0
 
 
--- lib/memory.mli
+++ lib/memory.mli
@@ -6,3 +6,4 @@ type long_register
 val reg: int -> register
 val reg_index: register -> int
 val long_reg : long_register
+val registers: register array
